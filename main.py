fdd2f36a287c3ab0008a295d54d385cb175b7cb5
Alexander Pershin <alexanderpershinwebdeveloper@gmail.com>
2025-09-10 14:44:55 +0700

    arguments support added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
+import argparse
 import logging
 import random
-import pygame
 from time import sleep
 
-log = logging.getLogger('sound_player')
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+log = logging.getLogger('SOUND_PLAYER')
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -15,23 +17,30 @@ class SoundPlayer:
 
     def __init__(
         self,
+        dir_name: str = 'sounds',
         delay_start: int = 5,
         delay_end: int = 10,
-        dir_name: str = 'sounds',
         show_log: bool = True,
     ):
+        if delay_end <= delay_start or delay_start < 0 or delay_end < 0:
+            raise ValueError('Delay start/end should be positive integers')
+
+        self._dir_name = dir_name
         self._delay_start = delay_start
         self._delay_end = delay_end
-        self._dir_name = dir_name
+
         if not show_log:
             log.propagate = False
 
     def __call__(self):
+        import pygame
+
         pygame.init()
         pygame.mixer.init()
         pygame.mixer.music.set_volume(1.0)
 
         filenames = os.listdir(self._dir_name)
+        log.info(f'reading directory: {self._dir_name}...')
         log.info(f'loaded files: {", ".join(filenames)}')
         try:
             while True:
@@ -47,7 +56,34 @@ class SoundPlayer:
 
 
 def main():
-    player = SoundPlayer()
+    parser = argparse.ArgumentParser(
+        description='A simple random sound playing program.'
+    )
+    parser.add_argument(
+        '--dir', type=str, default='sounds', help='Sounds direcotry'
+    )
+    parser.add_argument(
+        '--start', type=int, default=5, help='Sound play delay range start'
+    )
+    parser.add_argument(
+        '--end', type=int, default=10, help='Sound play delay range end'
+    )
+    parser.add_argument(
+        '--verbose', action='store_true', help='Show logs while running'
+    )
+
+    args = parser.parse_args()
+    dir_name = args.dir
+    delay_start = args.start
+    delay_end = args.end
+    verbose = args.verbose
+
+    player = SoundPlayer(
+        dir_name=dir_name,
+        delay_start=delay_start,
+        delay_end=delay_end,
+        show_log=verbose,
+    )
     player()
 
 
